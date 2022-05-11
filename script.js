ac41175a9ccdf834e362bb745bd7d58b8cab9a02
Mahima <mahimaandani@gmail.com>
2022-05-11 15:18:21 +0530

    created references

---
+++ script.js
@@ -0,0 +1,8 @@
+let button = document.querySelectorAll(".buttons");
+
+let fontName = document.getElementById("font-name-button");
+let fontSizeRef = document.getElementById("font-size-button");
+let writingArea = document.getElementById("content");
+let alignButtons = document.querySelectorAll(".align");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
