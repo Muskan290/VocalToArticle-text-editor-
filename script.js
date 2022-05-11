96f113bf662f5886b1c96ef079c03cbb948e3818
Muskan <muskandubey28112000@gmailcom>
2022-05-11 15:24:53 +0530

    highlighted buttons

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ let writingArea = document.getElementById("content");
 let alignButtons = document.querySelectorAll(".align");
 let formatButtons = document.querySelectorAll(".format");
 let scriptButtons = document.querySelectorAll(".script");
+let fontlist = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "cursive"
+]
+const initiliaser = () =>{
+  highlighter(alignButtons, true);
+  highlighter(formatButtons, true);
+  highlighter(scriptButton, true);
+
+}
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+      button.addEventListener("click", () => {
+        //needsRemoval = true means only one button should be highlight and other would be normal
+        if (needsRemoval) {
+          let alreadyActive = false;
+          //If currently clicked button is already active
+          if (button.classList.contains("active")) {
+            alreadyActive = true;
+          }
+          //Remove highlight from other buttons
+          highlighterRemover(className);
+          if (!alreadyActive) {
+            //highlight clicked button
+            button.classList.add("active");
+          }
+        } else {
+          //if other buttons can be highlighted
+          button.classList.toggle("active");
+        }
+      });
+    });
+  };
